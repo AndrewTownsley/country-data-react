2a5b61f2ffd6e30a5901cab6d115d854506e2cd7
Andrew TOwnsley <atownsley86@gmail.com>
2021-07-16 21:56:06 -0600

    sort function is working.  The problwm was that i was sorting the original countries array and rendering the samwe array to the DOM.  I needed to make a temp copy of the array, use the spread operator to add it back into the sort function, and set the state from there.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import App from './App';
-import AppClass from './AppClass';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <AppClass />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
