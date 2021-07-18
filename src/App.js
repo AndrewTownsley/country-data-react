76e92bdd63451bc15966b46778d85001e10f9c3e
Andrew TOwnsley <atownsley86@gmail.com>
2021-07-17 19:26:21 -0600

    region buttons are functional

--- src/App.js
+++ src/App.js
@@ -19,6 +19,58 @@ function App() {
     useEffect(() => {
       fetchCountry();
   }, []);
+  
+  const fetchEurope = () => {
+    fetch(`https://restcountries.eu/rest/v2/region/europe`)
+      .then(response => response.json())
+      .then(result => {
+        setCountries(result)
+      })
+      .catch((error) => console.log("error"))
+    }
+    
+    useEffect(() => {
+      fetchCountry();
+  }, []);
+
+  const fetchAfrica = () => {
+    fetch(`https://restcountries.eu/rest/v2/region/africa`)
+      .then(response => response.json())
+      .then(result => {
+        setCountries(result)
+      })
+      .catch((error) => console.log("error"))
+    }
+    
+    useEffect(() => {
+      fetchCountry();
+  }, []);
+
+  const fetchAmericas = () => {
+    fetch(`https://restcountries.eu/rest/v2/region/Americas`)
+      .then(response => response.json())
+      .then(result => {
+        setCountries(result)
+      })
+      .catch((error) => console.log("error"))
+    }
+    
+    useEffect(() => {
+      fetchCountry();
+  }, []);
+
+  const fetchAsia = () => {
+    fetch(`https://restcountries.eu/rest/v2/region/Asia`)
+      .then(response => response.json())
+      .then(result => {
+        setCountries(result)
+      })
+      .catch((error) => console.log("error"))
+    }
+    
+    useEffect(() => {
+      fetchCountry();
+  }, []);
 
   function sortPopulation() {
     let temp = [...countries]
@@ -56,7 +108,7 @@ function App() {
   }
 
   function resetCountryList() {
-    fetchCountry();
+    fetchEurope();
   }
 
   return (
@@ -66,6 +118,12 @@ function App() {
         <div className="dark-toggle">
           <button id="light" className="toggle-btn">Light</button>
           <button id="dark" className="toggle-btn">Dark</button>
+        </div>
+        <div className="region-btn-container">
+          <button onClick={fetchEurope} className="region-btn">Europe</button>
+          <button onClick={fetchAfrica} className="region-btn">Africa</button>
+          <button onClick={fetchAmericas} className="region-btn">Americas</button>
+          <button onClick={fetchAsia} className="region-btn">Asia</button>
         </div>
       </header>
       <section className="app-container">
