2ab4dd1f116fff4deeba55e231c8464971a6e6c0
Andrew TOwnsley <atownsley86@gmail.com>
2021-09-15 22:10:23 -0600

    CSS needs to be finished

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -6,55 +6,54 @@ const Detail = ({ countries, country }) => {
     const [countryDetail, setCountryDetail] = useState([]);
     const { name } = useParams()
 
-    useEffect(() => {
-        const fetchCountryData = async () => {
-            const response = await fetch(`https://restcountries.eu/rest/v2/name/${name}?fullText=true
-            `)
-            const country = await response.json();
-            setCountryDetail(country);
-            console.log(country);
-        }
-        fetchCountryData()
-    }, [name])
+    // useEffect(() => {
+    //     const fetchCountryData = async () => {
+    //         const response = await fetch(`https://restcountries.eu/rest/v2/name/${name}?fullText=true
+    //         `)
+    //         const country = await response.json();
+    //         setCountryDetail(country);
+    //         console.log(country);
+    //     }
+    //     fetchCountryData()
+    // }, [name])
 
+
+    useEffect(() => {
+        const fetchCountryData = () => {
+            fetch(`https://restcountries.eu/rest/v2/name/${name}?fullText=true`)
+            .then(response => response.json())
+            .then(result => setCountryDetail(result))
+        }
+        fetchCountryData();
+    },[name])
+    console.log(countryDetail);
+    
     return (
         <div className="detail-main">
             <div className="btn-container">
                 <Link to='/'><i className="fas fa-arrow-left">Back</i></Link>
             </div>
             {
-                countryDetail.map((item) => {
-                    const {
-                        name,
-                        flag,
-                        nativeName,
-                        population,
-                        capital,
-                        region,
-                        subregion,
-                        languages,
-                        currency,
-                        area,
-                        borders,
-                    } = item;
+                countryDetail.map((item) => 
+                    
 
-                    return  (
-                        <article className="detail-card">
-                            <img src={flag} alt={name} />
+                     (
+                        <article className="detail-card" key={item.numericCode}>
+                            <img src={item.flag} alt={item.name} />
                             <div className="detail-card-stats">
-                            <h1>{name}</h1>
-                               <h5>Native Name: <span className="country-stats">{nativeName}</span></h5>
-                               <h5>Population: <span className="country-stats">{population}</span></h5>
-                               <h5>Region: <span className="country-stats">{region}</span></h5>
-                               <h5>Sub Region: <span className="country-stats">{subregion}</span></h5>
-                               <h5>Capital: <span className="country-stats">{capital}</span></h5>
-                               <h5>Currencies: <span className="country-stats">{currency}</span></h5>
-                               <h5>Languages: <span className="country-stats">{languages}</span></h5>
-                               <h5>Area: <span className="country-stats">{area}</span></h5>
+                            <h1>{item.name}</h1>
+                               <h5>Native Name: <span className="country-stats">{item.nativeName}</span></h5>
+                               <h5>Population: <span className="country-stats">{item.population}</span></h5>
+                               <h5>Region: <span className="country-stats">{item.region}</span></h5>
+                               <h5>Sub Region: <span className="country-stats">{item.subregion}</span></h5>
+                               <h5>Capital: <span className="country-stats">{item.capital}</span></h5>
+                               <h5>Currencies: <span className="country-stats">{item.currencies[0].name}</span></h5>
+                               <h5>Languages: <span className="country-stats">{item.languages[0].name}</span></h5>
+                               <h5>Area: <span className="country-stats">{item.area}</span></h5>
                             </div>
                         </article>
                     )
-                })
+                )
             }
         </div>
     )
